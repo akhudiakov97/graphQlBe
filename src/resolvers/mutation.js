612210450d8daaefa4fbd705436129fff111b334
ahoodiakov97 <anton.khudiakov@greensill.com>
2020-02-11 22:57:38 +0200

    GraphQL BE HW

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,8 +1,11 @@
 // Mutations
-import { mutations as booksMutations } from '../bus/books/mutations';
-import { mutations as usersMutations } from '../bus/users/mutations';
+import {mutations as booksMutations} from '../bus/books/mutations';
+import {mutations as usersMutations} from '../bus/users/mutations';
+import {mutations as customersMutations} from '../bus/customers/mutations';
+
 
 export const Mutation = {
-  ...booksMutations,
-  ...usersMutations
+    ...booksMutations,
+    ...usersMutations,
+    ...customersMutations,
 };
